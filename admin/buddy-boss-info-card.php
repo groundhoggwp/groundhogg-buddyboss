467fd1244e3da6de356a51052cecd1986e5ccce5
Dhrumit <dhrumit07@gmail.com>
2021-02-17 14:46:33 -0500

    added section and groups.

--- admin/buddy-boss-info-card.php
+++ admin/buddy-boss-info-card.php
@@ -2,8 +2,10 @@
 
 namespace GroundhoggBuddyBoss\Admin;
 
+use BP_Groups_Member;
 use BP_XProfile_Data_Template;
 use BP_XProfile_Group;
+use Groundhogg\Contact;
 
 class Buddy_Boss_Info_Card {
 
@@ -19,7 +21,7 @@ class Buddy_Boss_Info_Card {
 	/**
 	 * Contact
 	 *
-	 * @param $contact
+	 * @param Contact $contact
 	 */
 	public function callback( $contact ) {
 		if ( ! $contact->get_user_id() ) {
@@ -28,15 +30,17 @@ class Buddy_Boss_Info_Card {
 			return;
 		}
 
-		printf( "<p><a href='%s' class='button secondary'>%s</a></p>", admin_url( 'users.php?page=bp-profile-edit&user_id=' . $contact->get_user_id() ), __( 'Edit Profile', 'groundhogg-buddyboss' ) );
+		echo "  <ul id='sortable'>";
+
+
+
+
 
 		$profile_template = new BP_XProfile_Data_Template( [
 			'user_id' => $contact->get_user_id(),
 		] );
 
-		foreach ( $profile_template->groups as $group ) {
-
-			echo "<h3>{$group->name}</h3>";
+		foreach ( $profile_template->groups as $group ) :
 
 			$group_data = BP_XProfile_Group::get( [
 				'user_id'                => $contact->get_user_id(),
@@ -53,35 +57,79 @@ class Buddy_Boss_Info_Card {
 				'update_meta_cache'      => true,
 			] );
 
-			echo " <table class=\"form-table\">";
-//                echo "<div class='content-info'>";
-//                echo "<ul>";
-			foreach ( $group_data[0]->fields as $field ) :
-				?>
-                <tr>
-                    <th><label><?php _e( $field->name, 'groundhogg-buddyboss' ) ?></label></th>
-                    <td><code><?php _e( $field->data->value, 'groundhogg-buddyboss' ) ?></code></td>
-                </tr>
+			?>
+            <ul class="container-info">
+                <div class="header-info">
+                    <div class="header-content">
+                        <span class="dashicons dashicons-businessman"></span><?php _e( 'Profile', 'groundhogg-buddyboss' ); ?>
+                    </div>
+                    <i class="dashicons dashicons-arrow-down-alt2"></i>
+                </div>
+                <div class="content-info">
+					<?php printf( "<p><a href='%s' class='button secondary'>%s</a></p>", admin_url( 'users.php?page=bp-profile-edit&user_id=' . $contact->get_user_id() ), __( 'Edit Profile', 'groundhogg-buddyboss' ) ); ?>
+                    <ul>
+						<?php foreach ( $group_data[0]->fields as $field ) : ?>
+                            <li>
+                                <div class="label"><?php _e( $field->name . ": ", 'groundhogg-buddyboss' ) ?></div>
+                                <div class="content"><?php _e( $field->data->value, 'groundhogg-buddyboss' ) ?></div>
+                            </li>
+						<?php endforeach; ?>
+                    </ul>
+                </div>
+            </ul>
+		<?php
 
-                <!---->
-                <!---->
-                <!--                    <li>-->
-                <!---->
-                <!--                        <div class="label">--><?php //_e( $field->name . ": ", 'groundhogg-buddyboss' )
-				?><!--</div>-->
-                <!---->
-                <!--                        <div class="content">--><?php //_e( $field->data->value, 'groundhogg-buddyboss' )
-				?><!--</div>-->
-                <!---->
-                <!--                    </li>-->
-                <!---->
+        endforeach;
 
-			<?php
-			endforeach;
-			echo "</table>";
-//				echo "</ul>";
-//				echo "</div>";
 
-		}
+        // Rendering the Groups
+
+        if (class_exists( 'BP_Groups_Member' ) ) :
+
+
+
+		$groups = BP_Groups_Member::get_group_ids( $contact->get_user_id() ) ['groups'];
+
+
+
+		?>
+
+
+        <ul class="container-info">
+            <div class="header-info">
+                <div class="header-content">
+                    <span class="dashicons dashicons-groups"></span><?php _e( 'Groups', 'groundhogg-buddyboss' ); ?>
+                </div>
+                <i class="dashicons dashicons-arrow-down-alt2"></i>
+            </div>
+            <div class="content-info">
+
+                <ul>
+					<?php
+                    if ($groups) :
+                    foreach ( $groups as $group_id ) :
+
+                       $group = groups_get_group($group_id);
+                        ?>
+                        <li>
+                            <div class="label"><?php _e( $group->name , 'groundhogg-buddyboss' ) ?></div>
+<!--                            <div class="content">--><?php //_e( $group->data->value, 'groundhogg-buddyboss' ) ?><!--</div>-->
+                        </li>
+					<?php endforeach;
+					else:
+						printf( "<p>%s</p>", __( "This contact is not part of any groups.", 'groundhogg-buddyboss' ) );
+                    endif;
+
+						?>
+                </ul>
+            </div>
+        </ul>
+
+
+		<?php
+
+        endif;
+		echo " </ul>";
+
 	}
 }
